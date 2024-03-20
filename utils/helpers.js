12c632f7f5fe20fd37238bfbe0b7a5e28ec1f755
Greg Yerkes <gyerkes@rajant.com>
2024-03-20 19:49:33 -0400

    working application except issue with login and no styling

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,6 +1,10 @@
+function formatDate(date) {
+  return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
+      date
+    ).getFullYear()}`;
+}
+
+
 module.exports = {
-    format_date: (date) => {
-      // Format date as MM/DD/YYYY
-      return date.toLocaleDateString();
-    },
+  formatDate
 }
